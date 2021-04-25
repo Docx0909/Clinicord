ddb1e14960d09ff4d70bdf39ab50e8ca3b182a9f
Gerald Tagle <65618262+Docx0909@users.noreply.github.com>
2021-04-25 22:09:01 +0800

    Pdf and Calendar Module
    Revision for Clinicord Web Application
    -3Discovery

---
+++ src/app/pages/pages/page4/resume.ts
@@ -0,0 +1,11 @@
+export class Report {
+    profilePic: string;
+    name: string;
+    address: string;
+    contactNo: number;
+    email: string;
+    appointmentdate: string;
+    healthDetails: string;
+    otherDetails: string;
+
+}
